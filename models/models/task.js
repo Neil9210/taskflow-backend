6e21fb2c294836fbf368fa998c1505c46fa9ee44
Nitin Mathur <ddnitin2002@gmail.com>
2026-02-22 19:02:05 +0530

    Delete models/models/task.js

--- models/models/task.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const taskSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  status: { type: String, default: "Pending" },
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
-}, { timestamps: true });
-
-module.exports = mongoose.model("Task", taskSchema);
