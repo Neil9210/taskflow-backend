2b07e07d33accbd411539753b94073968c86d7d8
Nitin Mathur <ddnitin2002@gmail.com>
2026-02-22 23:48:53 +0530

    Fix auth middleware token validation logic

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,18 +4,21 @@ module.exports = function (req, res, next) {
   try {
     const authHeader = req.header("Authorization");
 
-    if (!authHeader)
+    if (!authHeader) {
       return res.status(401).json({ msg: "No token" });
+    }
 
     // Remove "Bearer "
     const token = authHeader.split(" ")[1];
 
-    if (!token)
+    if (!token) {
       return res.status(401).json({ msg: "Invalid token" });
+    }
 
     const verified = jwt.verify(token, process.env.JWT_SECRET);
 
     req.user = verified.id;
+
     next();
   } catch (err) {
     res.status(401).json({ msg: "Invalid token" });
